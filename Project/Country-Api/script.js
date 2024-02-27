569bb655348cabb5702220433219554507edcaf9
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-02-27 15:23:43 +0530

    CHANGED JS

--- Project/Country-Api/script.js
+++ Project/Country-Api/script.js
@@ -4,10 +4,24 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request=new XMLHttpRequest();
 
+request.open('GET','https://restcountries.com/v3.1/name/india');
+request.send();
+request.addEventListener('load',function(){
 
-`
+const data=JSON.parse(this.responseText)[1];
+console.log(data);
 
+for(const lan in data.languages)
+{
+    console.log(data.languages[lan]);
+}
 
 
 })
+
+const data={
+    en:'english'
+}
+console.log(data['en']);
