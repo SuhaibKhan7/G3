6ed4775de5f6b01e65e05abf41985ac9bcc431c2
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-02-28 15:06:00 +0530

    js changed

--- Project/Country-Api/script.js
+++ Project/Country-Api/script.js
@@ -1,27 +1,54 @@
 
 
 const btn = document.querySelector('.btn-country');
+
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const request=new XMLHttpRequest();
 
-request.open('GET','https://restcountries.com/v3.1/name/india');
-request.send();
-request.addEventListener('load',function(){
-
-const data=JSON.parse(this.responseText)[1];
-console.log(data);
-
-for(const lan in data.languages)
+function getCountry(country)
 {
-    console.log(data.languages[lan]);
+    const request=new XMLHttpRequest();
+    
+    request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load',function(){
+    
+    const data=JSON.parse(this.responseText)[1];
+    console.log(data);
+    
+    let languages='';
+    for(const lan in data.languages){
+    
+        languages+=data.languages[lan]+' ';
+    }
+    console.log(languages);
+
+let currencies='';
+for(const cur in data.currencies)
+{
+    currencies=data.currencies[cur].name
 }
 
 
-})
 
-const data={
-    en:'english'
+    const card=`<article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${Math.floor((data.population)/1000000) } M </p>
+      <p class="country__row"><span>🗣️</span>${languages}</p>
+      <p class="country__row"><span>💰</span>${ currencies}</p>
+    </div>
+    </article>`
+    
+    countriesContainer.insertAdjacentHTML('beforeend',card);
+    
+    
+    
+    })
 }
-console.log(data['en']);
+
+getCountry('america');
+getCountry('india');
