c4e73d236bd7ff94077a307507c05ca2c4ea08c4
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-01 13:14:39 +0530

    NewTask added

--- React/group3/src/components/AddTask.js
+++ React/group3/src/components/AddTask.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {useState} from 'react'
 import './AddTask.css'
-export default function AddTask() {
+export default function AddTask({setTask})
+ {
+
 const[newtask,setnewTask]=useState('')
+
 function newTask(event)
 {
    setnewTask(event.target.value)
@@ -11,12 +14,25 @@ function delTask()
 {
     setnewTask('')
 }
+
+function addTask(e)
+{
+  e.preventDefault();
+  const createTask={
+    id:Math.floor(Math.random()*100),
+    name:newtask,
+    complete:false
+  }
+setTask([createTask])
+
+}
+
   return (
     <div className='addTask'>
     <form >
     <label htmlFor="">Task</label>
     <input type="text" name='newtask' id='newtask' onChange={newTask} value={newtask}/>
-    <button className='btn addtask'>Add Task</button>
+    <button className='btn addtask' onClick={addTask}>Add Task</button>
     <span className='btn cleartask' onClick={delTask}>Clear</span>
     </form>
         <h1>{newtask}</h1>
