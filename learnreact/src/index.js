0135e1f08fe5aa04e13f69b59a4b663b1c0d05cd
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-05 10:12:12 +0530

    learnreact2day

--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h1>My first web App</h1>
-    <p>This is the first paragraph of our website and we are learning react js</p>
-  </div>
-
+  
+      <App/>
 );
 
 
