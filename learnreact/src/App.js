8d24b26d3346301ee4dc6864cc43aa545409d3f1
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-08 15:03:39 +0530

    boostrap updated

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './component/Header'
-import TopSection from './component/TopSection'
-import Counter from './component/Counter'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export default function App() {
   return (
     <div>
-        <Header/>
-        <TopSection/>
-            <br />
-        <Counter />
+      <Header />
     </div>
   )
 }
