e8331dacbc6cfb7ff0d2b729ecdf19ebc64d5f4a
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-19 12:50:40 +0530

    useeffect and async use

--- learnreact/src/component/UseEffect.js
+++ learnreact/src/component/UseEffect.js
@@ -4,29 +4,46 @@ export default function UseEffect() {
 
   const [counter, setCounter] = useState(0);
   const [card, setCard] = useState([])
+  const [url, setUrl] = useState('http://localhost:3004/products')
+  // useEffect(() => {
 
+  //     fetch(url)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setCard(data)
+  //     })
+
+  // }, [url])
   useEffect(() => {
-    fetch('http://localhost:3004/products')
-      .then(res => res.json())
-      .then(data => {
-        setCard(data)
-      })
+    const getdata = async () => {
+      let response = await fetch(url)
+      let result = await response.json();
+      {
+        setCard(result)
+      }
+    }
+    getdata();
+  }, [url])
 
-  }, [])
+
+
+
 
   return (
     <div>
       <h1>Counter:{counter}</h1>
       <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>Inc</button>
+      <br />
+      <button onClick={() => setUrl('http://localhost:3004/products?instock=true')} >Instock</button>
+      <button className='bg-danger'>Outofstock</button>
+      <div className='bg-primary text-white m-4 p-3'>
+        {
+          card.map((c) => {
+            return <li key={c.id}> {c.id}-------{c.pname}-----{c.Price}----<span className={JSON.parse(c.instock) ? "bg-warning " : "bg-danger w-25 "}>{JSON.parse(c.instock) ? 'Instock' : 'outofstock'}</span></li>
+          })
+        }
 
-    <div className='bg-primary text-white'>
-  {
-    card.map((c)=>{
-      return <li key={c.id}> {c.id}-------{c.pname}-----{c.Price}----<span >{JSON.parse(c.instock)?'Instock':'outofstock'}</span></li>
-    })
-  }
-
-    </div>  
+      </div>
     </div>
   )
 }
