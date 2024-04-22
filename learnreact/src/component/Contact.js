2025c1b8d84254d0aaaab777cce20d84588400c0
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-04-22 22:59:18 +0530

    contactpageadded

--- learnreact/src/component/Contact.js
+++ learnreact/src/component/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
+    const nav = useNavigate()
+    return (
+        <div>Contact
+            <button className='bg-danger' onClick={() => nav(-1)}>Back</button>
+        </div>
+    )
 }
