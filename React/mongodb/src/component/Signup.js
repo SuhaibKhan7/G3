1083f0d4aecc194445ec774c4a34ac82daf548da
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-05-15 11:22:13 +0530

    update

--- React/mongodb/src/component/Signup.js
+++ React/mongodb/src/component/Signup.js
@@ -5,24 +5,43 @@ export default function Signup() {
         username: '',
         password: ''
     })
-   function handlesubmit()
-   {
-    
-   } 
+    async function handlesubmit(e) {
+        e.preventDefault();
+        const response = await fetch('http://localhost:4003/data', {
+            method: 'POST',
+            body: JSON.stringify(formdata),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        })
+        console.log(response);
+        const data = await response.json()
+        console.log(data);
+    }
+
     function inputdata(e) {
         e.preventDefault();
         setFormData({ ...formdata, [e.target.name]: e.target.value });
         console.log(formdata);
     }
 
+
+    async function getdata() {
+        const response = await fetch('http://localhost:4003/data')
+        console.log(response);
+        const data = await response.json()
+        console.log(data);
+
+    }
     return (
         <div>
             <form action="" onSubmit={handlesubmit}>
                 <input type="text" name="username" id="username" onChange={inputdata} />
-                <input type="password" name="password"  onChange={inputdata} />
+                <input type="password" name="password" onChange={inputdata} />
                 <button type='submit'>Save</button>
             </form>
-
+            <button onClick={getdata}>Getdata</button>
         </div>
     )
 }
--- React/nodeserver/index.js
+++ React/nodeserver/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const cors = require('cors');
 const server = express();
 
-server.get('/signup', (req, res) => {
-    console.log('....');
-    res.send('hey');
+//middlware
+server.use(cors())
+
+server.get('/data', (req, res) => {
+    res.send({ username: 'dileep' });
+
 })
+server.post('/data', (req, res) => {
+
+    req.send({msg:'data received'});
+})
+
+
+
 
 server.listen(4003, () => {
     console.log('server started');
