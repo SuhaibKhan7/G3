9a5fec7fe89a723501e4e10d4b99b0778639ae85
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-05-16 01:00:40 +0530

    Update index.js

--- React/nodeserver/index.js
+++ React/nodeserver/index.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 const server = express();
 
 //middlware
 server.use(cors())
-server.use(bodyParser);
+server.use(express.json())
+
 server.get('/data', (req, res) => {
-    res.send({ username: 'dileep' });
+    res.send({ username: 'dileep'});
 
 })
 server.post('/data', (req, res) => {
-    console.log(req.body)
-    res.send({ msg: 'data received' });
+    console.log(req.body.username)
+    res.send({ msg: 'data received',data:req.body});
 })
-
-
 
 
 server.listen(4003, () => {
